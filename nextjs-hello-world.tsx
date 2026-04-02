f5fd118de5844bf1b0d956fc1fc9ca1d7ec970a3
unknown <siddhantmane2006@gmail.com>
2026-04-02 15:09:54 +0530

    feat: add Next.js hello world test component

---
+++ nextjs-hello-world.tsx
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+      <h1>Hello World</h1>
+      <p>This is a test component for YesBoss verification.</p>
+    </div>
+  );
+}
